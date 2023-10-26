7e18ca2dcbe5acbe4b771401e76b1175f44caff6
Anderson Viana <andersonp.viana.contato@gmail.com>
2023-10-26 13:05:38 -0300

    feat: Adding findUserByEmail

--- src/main/java/com/tasklists/repositories/UserRepository.java
+++ src/main/java/com/tasklists/repositories/UserRepository.java
@@ -2,11 +2,12 @@ package com.tasklists.repositories;
 
 import com.tasklists.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserById(Long id);
 
-    Optional<User> findUserByEmail(String email);
+    UserDetails findByEmail(String email);
 }
